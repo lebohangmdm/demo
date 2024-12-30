2f7962173aac8dbc35f1373a8c4058cf617a3daf
lebohangmdm <lebohangmdm@gmail.com>
2024-12-30 10:14:01 +0200

    create featured section

---
+++ app/components/Featured.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CarouselSize from "@/app/components/CarouselSize";
+
+const Featured = () => {
+  return (
+    <section className="section-container">
+      <h4 className="text-sm uppercase text-blue-500  font-semibold  md:text-base">
+        Trusted by Leading Companies
+      </h4>
+      <CarouselSize />
+    </section>
+  );
+};
+
+export default Featured;
