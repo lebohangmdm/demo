c009f175a1bef89b6286a227eafd97ea7a8fd11e
lebohangmdm <lebohangmdm@gmail.com>
2025-01-14 12:16:42 +0200

    create companyNavLinks, rename NavLinks and update navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,45 +1,26 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import NavLinks from "./NavLinks";
+import ServicesNavLinks from "@/app/components/ServicesNavLinks";
 import Menu from "@/app/components/Menu";
 import Logo from "./Logo";
-// import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import CompanyNavLink from "@/app/components/CompanyNavLink";
 
 const Navbar = () => {
-  useEffect(() => {
-    const hash = window.location.hash.replace("#", "");
-    if (hash) {
-      const element = document.getElementById(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, []);
-
   return (
     <nav className="relative main-container h-14 sm:h-16 lg:h-20">
       <div className="flex items-center justify-between">
         <Logo width={250} height={250} />
-        <div className="hidden lg:flex items-center lg:gap-8 xl:gap-12">
-          <Link href={"/"} className="nav-link">
-            Home
-          </Link>
-          <Link href={"/#about"} className="nav-link">
-            About us
-          </Link>
-          <NavLinks />
-          <Link href={"/#why-choose-us"} className="nav-link">
-            Why Choose Us
-          </Link>
+        <div className="hidden lg:flex items-center lg:gap-8 xl:gap-10">
+          <CompanyNavLink />
+          <ServicesNavLinks />
           <Button
             variant={"default"}
             className="py-4 px-4 font-semibold hover:bg-black rounded-none  transition-all duration-200 "
             size={"lg"}
             asChild
           >
-            <Link href={"/#contact"}>Get a Quote</Link>
+            <Link href={"/contact-us"}>Get a Quote</Link>
           </Button>
         </div>
         <Menu />
---
+++ app/components/ServicesNavLinks.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { serviceLinks } from "@/lib/links";
+
+export default function ServicesNavLinks() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <div
+      className="relative inline-block z-30"
+      onMouseEnter={() => setIsOpen(true)} // Show on hover
+      onMouseLeave={() => setIsOpen(false)} // Hide on mouse leave
+    >
+      <Button
+        variant={"ghost"}
+        onClick={toggleMenu} // Toggle on click
+        className="relative nav-link hover:text-blue-500"
+        asChild
+      >
+        <Link href={"/services"}>
+          Services <ChevronDown />
+        </Link>
+      </Button>
+
+      {isOpen && (
+        <ul
+          className={`absolute bg-white flex flex-col gap-2 w-[250px] p-4 shadow-lg 
+            transition-all duration-300 ease-in-out 
+            ${isOpen ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"}
+            origin-top`}
+        >
+          {serviceLinks.map((link) => (
+            <Link
+              href={link.href}
+              key={link.id}
+              className="font-medium text-sm hover:text-blue-500 transition-colors duration-150"
+            >
+              {link.name}
+            </Link>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
